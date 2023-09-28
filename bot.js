1c0a5da4820b24b226e3a3704d268657188917a7
NielsvandenDries <niels@openmindedit.nl>
2023-09-28 10:09:46 +0200

    update

--- bot.js
+++ bot.js
@@ -1,26 +1,23 @@
-// Importeer de benodigde modules
 const { Client, GatewayIntentBits } = require('discord.js');
-require('dotenv').config(); // Laad de .env-variabelen
+require('dotenv').config();
 
-// Maak een nieuwe Discord-client
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-// Haal het bot-token op uit het .env-bestand
 const TOKEN = process.env.DISCORD_TOKEN;
 
-// Event-handler voor wanneer de bot is ingelogd
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-// Event-handler voor berichten die de bot ontvangt
 client.on('messageCreate', (message) => {
+  console.log(`Received message: ${message.content}`);
+  
   if (message.content === '!ping') {
+    console.log(`!ping command received.`);
     message.reply('Pong!');
   }
 });
 
-// Log de bot in met het opgehaalde token
 client.login(TOKEN);
