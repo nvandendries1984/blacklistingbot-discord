ab6a057284a01a496df0f960fb8f68a43d33cc71
NielsvandenDries <niels@openmindedit.nl>
2023-09-28 10:20:56 +0200

    update

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
+const winston = require('winston');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
@@ -7,15 +8,29 @@ const client = new Client({
 
 const TOKEN = process.env.DISCORD_TOKEN;
 
+// Configureer het logboekbestand
+const logger = winston.createLogger({
+  level: 'info', // Stel het logboekniveau in op 'info', maar je kunt dit aanpassen aan je behoeften.
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.File({ filename: 'bot.log' }), // Dit is het bestand waarin de loggegevens worden opgeslagen.
+  ],
+});
+
 client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+  logger.info(`Logged in as ${client.user.tag}`);
 });
 
 client.on('messageCreate', (message) => {
-  console.log(`Received message: ${message.content}`);
+  logger.info(`Received message: ${message.content}`);
   
   if (message.content === '!ping') {
-    console.log(`!ping command received.`);
+    logger.info('!ping command received.');
     message.reply('Pong!');
   }
 });
